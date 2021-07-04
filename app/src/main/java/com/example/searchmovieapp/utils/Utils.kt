7987af694c641d5455ec311b931495cf680da888
Evgeniy Chufarnov <evgeniychufarnov@gmail.com>
2021-07-04 21:59:04 +0300

    added const val

--- app/src/main/java/com/example/searchmovieapp/utils/Utils.kt
+++ app/src/main/java/com/example/searchmovieapp/utils/Utils.kt
@@ -2,5 +2,7 @@ package com.example.searchmovieapp.utils
 
 import com.example.searchmovieapp.entities.MovieEntity
 
+private const val INDEX_OF_LAST_YEAR_CHAR = 3
+
 val MovieEntity.releaseYear: String
-    get() = releaseDate.slice(0..3)
+    get() = releaseDate.slice(0..INDEX_OF_LAST_YEAR_CHAR)
